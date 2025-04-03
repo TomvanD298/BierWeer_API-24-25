3f5650b98bb00c2fdba23dacfba838cb68bf81f9
TomvanD298 <tom@realafford.nl>
2025-04-03 20:16:23 +0200

    Startup and Weather API

--- server/server.js
+++ server/server.js
@@ -4,28 +4,6 @@ import { logger } from '@tinyhttp/logger';
 import { Liquid } from 'liquidjs';
 import sirv from 'sirv';
 
-const data = {
-  'beemdkroon': {
-    id: 'beemdkroon',
-    name: 'Beemdkroon',
-    image: {
-      src: 'https://i.pinimg.com/736x/09/0a/9c/090a9c238e1c290bb580a4ebe265134d.jpg',
-      alt: 'Beemdkroon',
-      width: 695,
-      height: 1080,
-    }
-  },
-  'wilde-peen': {
-    id: 'wilde-peen',
-    name: 'Wilde Peen',
-    image: {
-      src: 'https://mens-en-gezondheid.infonu.nl/artikel-fotos/tom008/4251914036.jpg',
-      alt: 'Wilde Peen',
-      width: 418,
-      height: 600,
-    }
-  }
-}
 
 const engine = new Liquid({
   extname: '.liquid',
@@ -33,23 +11,41 @@ const engine = new Liquid({
 
 const app = new App();
 
+
 app
   .use(logger())
-  .use('/', sirv('dist'))
+  .use('/', sirv(process.env.NODE_ENV === 'development' ? 'client' : 'dist'))
   .listen(3000, () => console.log('Server available on http://localhost:3000'));
 
-app.get('/', async (req, res) => {
-  return res.send(renderTemplate('server/views/index.liquid', { title: 'Home', items: Object.values(data) }));
-});
-
-app.get('/plant/:id/', async (req, res) => {
-  const id = req.params.id;
-  const item = data[id];
-  if (!item) {
-    return res.status(404).send('Not found');
-  }
-  return res.send(renderTemplate('server/views/detail.liquid', { title: `Detail page for ${id}`, item }));
-});
+  app.get('/', async (req, res) => {
+    try {
+      const apiKey = process.env.OpenWeatherKey;
+      const city = 'Amsterdam';
+      const country = 'NL';
+  
+      // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      
+      const weatherData = await response.json();
+  
+      return res.send(renderTemplate('server/views/index.liquid', {
+        title: 'Home',
+        weather: {
+          city: weatherData.name,
+          temperature: Math.round(weatherData.main.temp),
+          description: weatherData.weather[0].description,
+          icon: `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`
+        }
+      }));
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      return res.status(500).send('Error fetching weather data');
+    }
+  });
+  
 
 const renderTemplate = (template, data) => {
   const templateData = {
