2996ac6ac0435c5262b8aa3ea78ebd90d8210fe4
TomvanD298 <tom@realafford.nl>
2025-04-23 11:03:22 +0200

    Detail cleanup

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import './index.css';
 import '../server/layouts/base.js';
 import '../server/components/bier/weatherBeerMatcher.js'
 import '../server/components/city/city.js'
-import '../server/components/city/city.js'
 import '../server/views/index.js'
 
 
